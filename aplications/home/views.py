3f1acd1b9c0d12db4659cb078b28e415eb2d2cb7
PalomaStephania <a20230204@utem.edu.mx>
2024-11-15 20:12:44 -0600

    Iniciando repo

--- aplications/home/views.py
+++ aplications/home/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
-from django.views.generic import TemplateView
-# Create your views here.
-
+# Vista basada en clase para mostrar una lista
 class IndexView(TemplateView):
-	#atributos de clase
-	template_name = 'home.html'
+    template_name = 'home.html'
+
+# Vista para probar una lista estática
+class PruebaLista(ListView):
+    template_name = 'lista.html'
+    queryset = ['A', 'B', 'C', 'D']
+    context_object_name = 'lista_prueba'
+
+class ArraysView(TemplateView):
+    template_name = "arrays.html"
+        nombres = ["Juan", "María", "Carlos", "Ana", "Luis"]
+        apellidos = ["Pérez", "González", "Ramírez", "López", "Martínez"]
+        edades = [25, 30, 22, 28, 35]
+        context_object_name = 'Arrays'
